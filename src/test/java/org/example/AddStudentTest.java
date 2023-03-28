bd7929bc24a66acc2037be06fb96e20441c1a40e
BarbaMatei <mateibarba@yahoo.com>
2023-03-28 16:14:09 +0300

    added more test

--- src/test/java/org/example/AddStudentTest.java
+++ src/test/java/org/example/AddStudentTest.java
@@ -44,39 +44,63 @@ public class AddStudentTest {
 
     @Test
     void testAddStudent_ValidStudent_StudentAddedCorrectly() {
-        Student newStudent = new Student("1111", "Matei", 931);
-        service.saveStudent("1111", "Matei", 931);
+        Student newStudent = new Student("1", "Alex", 931);
+        service.saveStudent("1", "Alex", 931);
         assertEquals(newStudent, service.findAllStudents().iterator().next());
     }
 
     @Test
-    void testAddStudent_StudentGroupTooSmall() {
-        assertEquals(1, service.saveStudent("113", "a", 10));
+    void testAddStudent_InvalidStudent_StudentGroupTooSmall() {
+        assertEquals(1, service.saveStudent("2", "Alex", -2));
     }
 
     @Test
-    public void testAddStudent_StudentGroupTooLarge() {
-        assertEquals(1, service.saveStudent("114", "a", 100000));
+    public void testAddStudent_InvalidStudent_StudentGroupTooLarge() {
+        assertEquals(1, service.saveStudent("3", "a", 100000));
     }
 
     @Test
-    public void testAddStudent_EmptyStudentName() {
-        assertEquals(1, service.saveStudent("115", "", 931));
+    public void testAddStudent_InvalidStudent_EmptyStudentName() {
+        assertEquals(1, service.saveStudent("4", "", 931));
     }
 
     @Test
-    public void testAddStudent_NullStudentName() {
-        assertEquals(1, service.saveStudent("116", null, 931));
+    public void testAddStudent_InvalidStudent_NullStudentName() {
+        assertEquals(1, service.saveStudent("5", null, 931));
     }
 
     @Test
-    public void testAddStudent_EmptyStudentID() {
+    public void testAddStudent_InvalidStudent_EmptyStudentID() {
         assertEquals(1, service.saveStudent("", "a", 931));
 
     }
 
     @Test
-    public void testAddStudent_NullStudentID() {
+    public void testAddStudent_InvalidStudent_NullStudentID() {
         assertEquals(1, service.saveStudent(null, "a", 931));
+    }
+
+    @Test
+    void testAddStudent_InvalidStudent_GroupBelowLowerBoundary() {
+        assertEquals(1, service.saveStudent("6", "Alex", 109));
+    }
+
+    @Test
+    void testAddStudent_ValidStudent_GroupEqualToLowerBoundary() {
+        Student newStudent = new Student("7", "Alex", 110);
+        service.saveStudent("7", "Alex", 931);
+        assertEquals(newStudent, service.findAllStudents().iterator().next());
+    }
+
+    @Test
+    void testAddStudent_InvalidStudent_GroupAboveHigherBoundary() {
+        assertEquals(1, service.saveStudent("8", "Alex", 939));
+    }
+
+    @Test
+    void testAddStudent_ValidStudent_GroupEqualToHigherBoundary() {
+        Student newStudent = new Student("9", "Alex", 938);
+        service.saveStudent("7", "Alex", 931);
+        assertEquals(newStudent, service.findAllStudents().iterator().next());
     }
 }
