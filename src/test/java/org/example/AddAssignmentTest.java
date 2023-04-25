9ff93b5fac7949ad9a2021719206104e83152f85
BarbaMatei <mateibarba@yahoo.com>
2023-04-25 09:49:26 +0300

    added more test

--- src/test/java/org/example/AddAssignmentTest.java
+++ src/test/java/org/example/AddAssignmentTest.java
@@ -59,7 +59,54 @@ public class AddAssignmentTest {
     }
 
     @Test
-    void testAddTema_InvalidTema_StartLineLargerThanDeadline() {
+    void testAddTema_InvalidTema_StartlineLargerThanDeadline() {
         assertEquals(1, service.saveTema("2", "Alex", 10, 11));
     }
+
+    @Test
+    void testAddTema_ValidTema_StartlineBeforeDeadline() {
+        Tema newTema = new Tema("21", "aa", 4, 3);
+        service.saveTema("21", "aa", 4,3);
+        assertEquals(newTema, service.findAllTeme().iterator().next());
+        service.deleteTema("21");
+    }
+
+    @Test
+    void testAddTema_InvalidTema_Startline_0() {
+        assertEquals(1, service.saveTema("22", "aa", 2, 0));
+    }
+
+    @Test
+    void testAddTema_ValidTema_Deadline_13() {
+        Tema newTema = new Tema("23", "aa", 13,12);
+        service.saveTema("23", "aa", 13,12);
+        assertEquals(newTema, service.findAllTeme().iterator().next());
+        service.deleteTema("23");
+    }
+
+    @Test
+    void testAddTema_InvalidTema_Deadline_15() {
+        assertEquals(1, service.saveTema("24", "aa", 15, 12));
+    }
+
+    @Test
+    void testAddTema_InvalidTema_Startline_and_Deadline_negative() {
+        assertEquals(1, service.saveTema("25", "aa", -2,-1));
+    }
+
+    @Test
+    void testAddTema_InvalidTema_Startline_20_and_Deadline_25() {
+        assertEquals(1, service.saveTema("26", "aa", 25, 20));
+    }
+
+    @Test
+    void testAddTema_InvalidTema_Id_emptyString() {
+        assertEquals(1, service.saveTema("", "aa", 6,5));
+    }
+
+    @Test
+    void testAddTema_Descriere_emptyString() {
+        assertEquals(1, service.saveTema("27", "", 6, 5));
+    }
+
 }
